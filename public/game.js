8c7ef27ad2e37f1a4e0b9dd226e6a8f805778281
Dosybi <dosybi@gmail.com>
2024-02-28 12:14:04 +0600

    add fullscreen test

--- public/game.js
+++ public/game.js
@@ -23,6 +23,16 @@ let score = 0
 let lives = 3
 let isGameOver = false
 
+if (document.documentElement.requestFullscreen) {
+  document.documentElement.requestFullscreen()
+} else if (document.documentElement.mozRequestFullScreen) {
+  document.documentElement.mozRequestFullScreen()
+} else if (document.documentElement.webkitRequestFullscreen) {
+  document.documentElement.webkitRequestFullscreen()
+} else if (document.documentElement.msRequestFullscreen) {
+  document.documentElement.msRequestFullscreen()
+}
+
 function createCook() {
   const cookElement = document.createElement('div')
   cookElement.innerHTML = '👩‍🍳'
