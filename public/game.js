110f1657c6e84d36fed59636d5b360ea26b9c7d9
Dosybi <dosybi@gmail.com>
2024-03-07 13:37:20 +0500

    fix number of lives

--- public/game.js
+++ public/game.js
@@ -294,7 +294,7 @@ function updateCookPosition(offset) {
 
 function initGame() {
   score = 0
-  lives = 1
+  lives = 5
   ingredientFallSpeed = 10
   ingredientCreationInterval = 2000
   speedIncreaseThreshold = 50
